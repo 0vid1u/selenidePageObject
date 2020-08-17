c952347e49ddb46a86b64bf5620bae4fc73fb718
Siarhei Manko <siarhei.manko@order2cash.com>
2020-08-17 10:26:52 +0200

    added some additional elements

--- src/test/java/net/bugoff/selenide/steps/Hooks.java
+++ src/test/java/net/bugoff/selenide/steps/Hooks.java
@@ -7,5 +7,12 @@ public class Hooks {
     @Before
     public void start(){
         Configuration.startMaximized = true;
+
+//  Just to show some configuration examples
+//        Configuration.timeout = 5000;
+//        Configuration.headless = true;
+//        Configuration.baseUrl = "www.example.com";
+//        Configuration.browser = "firefox";
+//        Configuration.downloadsFolder = "src/test/resources/download";
     }
 }
